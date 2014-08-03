8b64ecfde99e6656a3fa28e6e4a0646f6452eb03
Pooya Deldar Gohardani <pooya@linux-km9l.site>
2014-08-03 09:58:51 +0430

    Overlay is added to project thumbnails

--- product-resume.js
+++ product-resume.js
@@ -12,10 +12,20 @@ $(function () {
     });
 
 
-    var currentHeader = $(this).find(".navbar");
-    if (currentHeader.length == 0)
-        return;
-    makeSticky($(this), currentHeader);
+    $('.project').hover(
+        function(){
+            $(this).find('.project-overlay').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.project-overlay').slideUp(250); //.fadeOut(205)
+        }
+    );
+
+
+//    var currentHeader = $(this).find(".navbar");
+//    if (currentHeader.length == 0)
+//        return;
+//    makeSticky($(this), currentHeader);
 
 
 });
